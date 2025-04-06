22446976336320e93c1245b773feec58c78b8b12
praveen006h <praveenppk2004@gmail.com>
2025-04-06 15:54:17 +0530

    reordered DB files

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -26,16 +26,17 @@ async function main() {
   const dataDirectory = path.join(__dirname, "seedData");
 
   const orderedFileNames = [
-    "products.json",
-    "expenseSummary.json",
     "sales.json",
     "salesSummary.json",
     "purchases.json",
     "purchaseSummary.json",
-    "users.json",
     "expenses.json",
     "expenseByCategory.json",
+    "expenseSummary.json",
+    "products.json",
+    "users.json"
   ];
+  
 
   await deleteAllData(orderedFileNames);
 
