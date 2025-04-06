f6c49cd359d94d5f261a6750e652d94ca8998a3c
praveen006h <praveenppk2004@gmail.com>
2025-04-06 10:37:35 +0530

    initial commit

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import {
+  CheckCircle,
+  Package,
+  Tag,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
+import CardExpenseSummary from "./CardExpenseSummary";
+import CardPopularProducts from "./CardPopularProducts";
+import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardSalesSummary from "./CardSalesSummary";
+import StatCard from "./StatCard";
+
+const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+      <CardSalesSummary />
+      <CardPurchaseSummary />
+      <CardExpenseSummary />
+      
+    </div>
+  );
+};
+
+export default Dashboard;
